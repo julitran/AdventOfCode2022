ff65b6d1510824a7b7f143800fb3df001cc87c56
julitran <juliee.trann@gmail.com>
2022-12-18 21:39:24 -0700

    forgot one file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    RockPaperScissorsStrategizer rps = new RockPaperScissorsStrategizer();
+        int tally = rps.rockPaperScissorFileReader();
+        System.out.println(tally);
+    }
+}
