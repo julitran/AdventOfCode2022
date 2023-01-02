5cb53a7833df9323487aa75c40da34077fda9a04
julitran <juliee.trann@gmail.com>
2023-01-01 21:32:07 -0700

    Completed day 3

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,29 @@
 import day1.CalorieCounter;
 import day2.RockPaperScissorsStrategizer;
+import day3.RuckSackOrganizer;
 import day4.CampCleanUp;
 import day5.SupplyStack;
 import day6.CommunicationMarker;
+import day7.DirectorySize;
 
 public class Main {
 
     public static void main(String[] args) {
 
-/*        //day 1
-        CalorieCounter calorieCounter = new CalorieCounter();
+        //day 1
+/*        CalorieCounter calorieCounter = new CalorieCounter();
         System.out.println("Day 1: " + calorieCounter.getHighestCalorieCountOfTopReindeers(3));
 
         //day 2
 	    RockPaperScissorsStrategizer rps = new RockPaperScissorsStrategizer();
-        System.out.println("Day 2: " + rps.rockPaperScissorFileReader());
+        System.out.println("Day 2: " + rps.rockPaperScissorFileReader());*/
 
-        //day
+        //day 3
+        RuckSackOrganizer rso = new RuckSackOrganizer();
+        System.out.println("Day 3: " + rso.readFilepart2());
 
         //day 4
-        CampCleanUp ccu = new CampCleanUp();
+/*        CampCleanUp ccu = new CampCleanUp();
         System.out.println("Day 4: " + ccu.containsAssignmentPairCount());
 
         //day 5
@@ -27,8 +31,12 @@ public class Main {
         System.out.println("Day 5:" + ss.readFile());*/
 
         //day 6
-        CommunicationMarker cm = new CommunicationMarker();
-        System.out.println("Day 6:" +  cm.getCommunicationMarkerIndex(14));
+        //CommunicationMarker cm = new CommunicationMarker();
+        //System.out.println("Day 6: " +  cm.getCommunicationMarkerIndex(14));
+
+        //day 7
+        //DirectorySize ds = new DirectorySize();
+        //System.out.println("Day 7: " +  ds.readFile());
 
 
     }
--- src/day3/RuckSackOrganizer.java
+++ src/day3/RuckSackOrganizer.java
@@ -1,4 +1,95 @@
 package day3;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 public class RuckSackOrganizer {
+
+    int _priorityLevel = 0;
+
+    public String readFilepart1() {
+        try {
+            File fileName = new File("src/inputFiles/day3.txt");
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String st;
+            _priorityLevel = 0;
+
+            while ((st = bufferedReader.readLine()) != null) {
+                int rucksackLength = st.length();
+                int compartmentLength = rucksackLength / 2;
+                String compartmentOne = st.substring(0, compartmentLength);
+                String compartmentTwo = st.substring(compartmentLength, rucksackLength);
+
+
+                for(int i =0; i < compartmentLength; i ++){
+                    char a = compartmentOne.charAt(i);
+                    if(compartmentTwo.contains(String.valueOf(a))){
+                        _priorityLevel += checkPriority(a);
+                        break;
+                    }
+                }
+
+            }
+            return String.valueOf(_priorityLevel);
+
+        }catch(Exception e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+        return String.valueOf(0);
+    }
+
+    private int checkPriority(char c) {
+        return (Character.isUpperCase(c)) ? ( c - 38): (c - 96);
+    }
+
+    public String readFilepart2() {
+        try {
+            File fileName = new File("src/inputFiles/day3.txt");
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+            String st;
+            _priorityLevel = 0;
+            List<String> item = new ArrayList<>(3);
+
+
+            while ((st = bufferedReader.readLine()) != null) {
+                item.add(st);
+                if(item.size() == 3) {
+                    char badge = findBadge(item);
+                    _priorityLevel += checkPriority(badge);
+                    item.clear();
+                }
+            }
+            return String.valueOf(_priorityLevel);
+
+        }catch(Exception e){
+            System.out.println(Arrays.toString(e.getStackTrace()));
+        }
+        return String.valueOf(0);
+    }
+
+    private char findBadge(List<String> item) {
+        String sequenceOne = item.get(0);
+        String sequenceTwo = item.get(1);
+        String sequenceThree = item.get(2);
+        char a = 'a';
+
+       int maxLength = getMaxLength(sequenceOne.length(), sequenceTwo.length(), sequenceThree.length());
+
+        for(int i =0; i < maxLength; i ++){
+            a = sequenceOne.charAt(i);
+            if(sequenceTwo.contains(String.valueOf(a)) && sequenceThree.contains(String.valueOf(a))){
+               break;
+            }
+        }
+        return a;
+    }
+
+    private int getMaxLength(int a, int b, int c) {
+        return Math.max(a, Math.max(b, c));
+    }
 }
